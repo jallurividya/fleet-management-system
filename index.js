d88b008ab6169ffc5e802df54ac621680881e06e
jallurividya <jallurividya2002@gmail.com>
2026-01-31 20:14:59 +0530

    evaluation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-import { checkDBConnection } from "./src/utils/dbHealthCheck.js"
+import { checkDBConnection } from "./utils/dbHealthCheck.js"
 dotenv.config()
 const app = express()
 app.use(express.json())
--- utils/dbHealthCheck.js
+++ utils/dbHealthCheck.js
@@ -1,7 +1,7 @@
 import { supabse } from "../config/supabaseConfig.js";
 export async function checkDBConnection() {
     try{
-        const { error } = await supabse.from("user").select().limit(1);
+        const { error } = await supabse.from("users").select().limit(1);
         if(error) throw error
         console.log("Database connected successfully");
         return true;
